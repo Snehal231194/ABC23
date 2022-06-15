bd923e138f12738cf0cde71a9d62c131f0aac8c8
SONY <SONY@DESKTOP-PUTEV6N>
2022-06-15 12:46:54 +0530

    By Priya

--- GIT1/src/Com/cjc/demo1.java
+++ GIT1/src/Com/cjc/demo1.java
@@ -1,7 +1,14 @@
 package Com.cjc;
 
+import org.omg.CORBA.PUBLIC_MEMBER;
+
 public class demo1 {
+	public void m1()
+	{
+		System.out.println("m1");
+	}
 public static void main(String[] args) {
 	System.out.println("GIT");
+	
 }
 }
