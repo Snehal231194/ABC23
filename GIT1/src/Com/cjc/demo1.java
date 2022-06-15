00bb925d98a615fb1b256cebda64758307c8c876
Snehal231194 <107501514+Snehal231194@users.noreply.github.com>
2022-06-15 12:49:01 +0530

    Update demo1.java

--- GIT1/src/Com/cjc/demo1.java
+++ GIT1/src/Com/cjc/demo1.java
@@ -7,6 +7,10 @@ public class demo1 {
 	{
 		System.out.println("m1");
 	}
+	public void m2()
+	{
+		System.out.println("m2");
+	}
 public static void main(String[] args) {
 	System.out.println("GIT");
 	
